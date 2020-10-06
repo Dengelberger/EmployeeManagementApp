6aba96ef6d6c5dd5b4756bb718c982395a2a1fc8
Donna Engelberger <dengelberger@gmail.com>
2020-10-06 09:11:37 -0400

    as of end of day on Oct 5

--- index.js
+++ index.js
@@ -1,63 +1,106 @@
 const inquirer = require("inquirer");
+var mysql = require("mysql");
 const path = require("path");
 const fs = require("fs");
-const { type } = require("os");
+const cTable = require("console.table");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 
-var employees = [];
 
-//Prompts used to build the database:
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Bax&Nel2020",
+    database: "employee_db"
+});
 
-//Inquirer Prompt #1 THIS IS THE INITIAL QUESTION FOR THE APP TO RUN.
+connection.connect(function (err) {
+    if (err)
+        throw err;
 
-inquirer.prompt([
-    {
-        name: "type",
-        message: "What would you like to do?",
-        type: "choice",
-        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add New Employee", "Delete Employee", "Update Employee Role", "Update Employee Manager"]
-    }
+    console.log("connected as id " + connection.threadId);
 
-]).then
+    updateEmployeeDB();
+});
 
-if (choice[0]) {
-    displayAllEmployees();
-} else if (choice[1]) {
-    displayEmployeesByDepartment();
-} else if (choice[2]) {
-    displayEmployeesByManager();
-} else if (choice[3]) {
-    addNewEmployees();
-} else if (choice[4]) {
-    deleteEmployee();
-} else if (choice[5]) {
-    updateEmployeeRole();
-} else if (choice[6]) {
-    updateEmployeeManager();
-} throw (error);
+const updateEmployeeDB = () => {
 
+    //Prompts used to build the database:
+
+    //Inquirer Prompt #1 THIS IS THE INITIAL QUESTION FOR THE APP TO RUN.
+
+    inquirer.prompt([
+        {
+            name: "menuChoice",
+            message: "What would you like to do?",
+            type: "list",
+            choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add New Employee", "Delete Employee", "Update Employee Role", "Update Employee Manager", "Quit"]
+        }
+
+    ]).then(({ menuChoice }) => {
+        switch (menuChoice) {
+            case "View All Employees":
+                displayAllEmployees();
+                break;
+            case "View All Employees by Department":
+                displayEmployeesByDepartment();
+                break;
+            case "View All Employees by Manager":
+                displayEmployeesByManager();
+                break;
+            case "Add New Employee":
+                addNewEmployees();
+                break;
+            case "Delete Employee":
+                deleteEmployee();
+                break;
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+            case "Update Employee Manager":
+                updateEmployeeManager();
+                break;
+            default:
+                connection.end();
+        }
+    });
+
+};
 //IF CHOICE [0], DISPLAY ALL EMPLOYEES CHART ON SCREEN.
 
-function displayAllEmployees(); {
+const displayAllEmployees = ()=> {
+    
+    console.table({
 
+    })
 };
 
 //IF CHOICE [1], DISPLAY ALL EMPLOYEES SORTED BY DEPARTMENT ON SCREEN.
 
-function displayEmployeesByDepartment() {
+const displayEmployeesByDepartment = () => {
 
+    console.table({
+
+    })
 };
 
 //IF CHOICE [2], DISPLAY ALL EMPLOYEES SORTED BY MANAGER ON THE SCREEN.
 
-function displayEmployeesByManager(); {
+const displayEmployeesByManager = () => {
+
+    console.table({
+
+        
+    })
 
 };
 
 //IF CHOICE [3] Inquirer Prompt #2 THIS IS TO ADD A NEW EMPLOYEE.
 
-function addNewEmployees() {
+var employeeRoles = ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"];
+
+const addNewEmployees = () => {
 
     inquirer.prompt([
         {
@@ -75,79 +118,155 @@ function addNewEmployees() {
         {
             name: "role",
             message: "What is the new employee's role?",
-            type: "choice",
-            choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"] //IF THE USER CHOICE IS ITEM [0] OR ITEM [2] OR ITEM [5] OR ITEM [6], THEN THERE SHOULD BE NO CHOICE FOR MANAGER. ALSO, ONE OF THESE FOUR CHOICES SHOULD PUT THIS EMPLOYEE IN A MANAGER CLASS.
+            type: "list",
+            choices: employeeRoles,
+            default: "none"
         }
-    ]).then 
-    
-    if (role[1] || role[3] || role[4]) {
-    
-            //THIS QUESTION SHOULD ONLY COME UP IF ITEM [1], [3], OR [4] IS CHOSEN, ABOVE.
-    inquirer.prompt({
-            name: "manager",
-            message: "Who is the new employee's manager?",
-            type: "choice",
-            choices: []  //these will be provided from the existing list of managers, plus a choice of none.
+    ])
+}.then(addManager => {
+    connection.query(`SELECT "Sales Lead", "Lead Engineer", "Accountant", "Legal Team Lead" FROM employees.role`, function (err, managerData) {
+        if (err)
+            throw err;
+        if (managerData.length > 0) {
+
+            const managerNames = managerData.map(item = item.name);
+
+            inquirer.prompt([
+                {
+                    name: "manager_name",
+                    message: "Who will be this employee's manager?",
+                    type: "list",
+                    choices: managerNames,
+                    default: "none"
+                }
+        }
     }
-    )};
-    //AT THIS POINT WE WANT TO SEND THE INFORMATION TO THE DB AND POPULATE THE EMPLOYEES TABLE.
-};
+    ]).then(postAnswers => {
+        connection.query("INSERT INTO employees(first_name, last_name, role) VALUES(?, ?, ?)", [postAnswers.first_name, postAnswers.last_name, postAnswers.role], function (err, postData) {
+            if (err)
+                throw err;
+            console.log("new employee has successfully been added.");
+            console.log(postAnswers);
+
+            updateEmployeeDB();
+        });
+    });
+}
 
 //IF CHOICE [4] Inquirer Prompt #3 THIS IS TO REMOVE AN EXISTING EMPLOYEE.
 
-function deleteEmployee() {
+const deleteEmployee = () => {
 
-    inquirer.prompt([
-        {
-            name: "name",
-            message: "Which employee do you wish to delete?",
-             type: "choice",
-            choices: [] //this will be populated with the entire list of current employees.
-        }
-    ]) //AT THIS POINT WE WANT TO SEND THE INFORMATION TO THE DB TO UPDATE THE EMPLOYEES TABLE TO EXCLUDE THIS EMPLOYEE.
-};
+    connection.query("SELECT * FROM employees", function (err, employeeData) {
+        if (err)
+            throw err;
 
-//IF CHOICE [5] Inquirer Prompt #4  THIS PROMPT IS TO UPDATE AN EMPLOYEE'S ROLE.
+        // console.log(productData);
 
-function updateEmployeeRole() {
+        if (employeeData.length > 0) {
 
-    inquirer.prompt([
-        {
-            name: "name",
-            message: "Which employee do you wish to update?",
-            type: "choice",
-            choices: [] //this will be populated with the entire list of current employees.
-        },
+            const employeeNames = employeeData.map(item => item.name);
 
-        {   //THIS IS THE SAME PROMPT AS WHEN ADDING A NEW EMPLOYEE.
-            name: "role",
-            message: "What is the new employee's role?",
-            type: "choice",
-            choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"] //IF THE USER CHOICE IS ITEM [0] OR ITEM [2] OR ITEM [5] OR ITEM [6], THEN THERE SHOULD BE NO CHOICE FOR MANAGER. ALSO, ONE OF THESE FOUR CHOICES SHOULD PUT THIS EMPLOYEE IN A MANAGER CLASS.
-        }
-    ])//AT THIS POINT WE WANT TO SEND THE NEW INFORMATION TO THE DB TO UPDATE THIS EMPLOYEE'S ROLE IN THE EMPLOYEES TABLE.
-};
+            inquirer.prompt([
+                {
+                    name: "nameToDelete",
+                    message: "Which employee do you wish to delete?",
+                    type: "choice",
+                    choices: employeeNames,
+                    default: "none"
+                }
+                //AT THIS POINT WE WANT TO SEND THE INFORMATION TO THE DB TO UPDATE THE EMPLOYEES TABLE TO EXCLUDE THIS EMPLOYEE.
+            ]).then(postAnswers => {
+                connection.query("DELETE FROM employees(first_name, last_name, role) VALUES(?, ?, ?)", [postAnswers.first_name, postAnswers.last_name, postAnswers.role], function (err, postData) {
+                    if (err)
+                        throw err;
+                    console.log(`${nameToDelete} has been deleted.`);
+                    console.log(postAnswers);
 
-    //IF CHOICE [6] Inquirer Prompt #5  THIS PROMPT IS TO UPDATE AN EMPLOYEE'S MANAGER.
+                    updateEmployeeDB();
+                });
+            });
+        };
+    });
 
-function updateEmployeeManager() {
+    // IF CHOICE [5] Inquirer Prompt #4  THIS PROMPT IS TO UPDATE AN EMPLOYEE'S ROLE.
 
-    inquirer.prompt([
-        {   //THIS IS THE SAME PROMPT AS WHEN UPDATING AN EMPLOYEE'S ROLE.
-            name: "name",
-            message: "Which employee do you wish to update?",
-            type: "choice",
-            choices: [] //this will be populated with the entire list of current employees.
-        },
+    const updateEmployeeRole = () => {
+        connection.query("SELECT * FROM employees", function (err, employeeData) {
+            if (err)
+                throw err;
 
-        {   //THIS IS THE SAME PROMPT AS WEHN ADDING A NEW EMPLOYEE.
-            name: "manager",
-            message: "Who is the new employee's manager?",
-            type: "choice",
-            choices: []  //these will be provided from the existing list of managers, plus a choice of none.
-        }
+            // console.log(productData);
 
-    ])//AT THIS POINT WE WANT TO SEND THE NEW INFORMATION TO THE DB TO UPDATE THIS EMPLOYEE'S MANAGER IN THE EMPLOYEES TABLE.
-};
+            if (employeeData.length > 0) {
 
-//THIS IS THE END OF THE PROMPTS.
+                const employeeNames = employeeData.map(item => item.name);
+
+                inquirer.prompt([
+                    {
+                        name: "name",
+                        message: "Which employee do you wish to update?",
+                        type: "choice",
+                        choices: employeeNames,
+                        default: "none"
+                    },
+
+                    {   //THIS IS THE SAME PROMPT AS WHEN ADDING A NEW EMPLOYEE.
+                        name: "role",
+                        message: "What is the new employee's role?",
+                        type: "choice",
+                        choices: employeeRoles,
+                        default: "none"
+                    }
+                    //AT THIS POINT WE WANT TO SEND THE NEW INFORMATION TO THE DB TO UPDATE THIS EMPLOYEE'S ROLE IN THE EMPLOYEES TABLE.
+                ]).then(postAnswers => {
+                    connection.query("UPDATE employees(first_name, last_name, role) VALUES(?, ?, ?)", [postAnswers.first_name, postAnswers.last_name, postAnswers.role], function (err, postData) {
+                        if (err)
+                            throw err;
+                        console.log("This employee's role  has successfully been updated.");
+                        console.log(postAnswers);
+
+                        updateEmployeeDB();
+                    });
+                });
+            };
+
+//IF CHOICE [6] Inquirer Prompt #5  THIS PROMPT IS TO UPDATE AN EMPLOYEE'S MANAGER.
+
+const updateEmployeeManager = () => {
+    connection.query("SELECT * FROM employees", function (err, employeeData) {
+        if (err)
+            throw err;
+        if (employeeData.length > 0) {
+
+            const employeeNames = employeeData.map(item => item.name);
+
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "Which employee do you wish to update?",
+                    type: "choice",
+                    choices: employeeNames,
+                    default: "none"
+                },
+
+                {
+                    name: "manager",
+                    message: "Who is the new employee's manager?",
+                    type: "choice",
+                    choices: managerNames,
+                    default: "none"
+                }
+
+            ]).then(postAnswers => {
+                connection.query("UPDATE employees(first_name, last_name, role) VALUES(?, ?, ?)", [postAnswers.first_name, postAnswers.last_name, postAnswers.role], function (err, postData) {
+                    if (err)
+                        throw err;
+                    console.log("This employee's role  has successfully been updated.");
+                    console.log(postAnswers);
+
+                    updateEmployeeDB();
+                });
+            });
+        };
+
